181deb1fbf47174f4f8b83a2aecc9048c0aa4653
Bette Anjanelle <163548141+anjobette@users.noreply.github.com>
2025-06-04 16:21:04 +0800

    created loading component

---
+++ src/components/loading.tsx
@@ -0,0 +1,22 @@
+import "@/styles/loading.css"
+
+export default function Loading() {
+    return (
+        <div className="fetch-container">
+            <div className="loading-box">
+                <div className="wheel"></div>
+                <div className="wheel"></div>
+            </div>
+
+            <p className="loading-text">
+                <span>L</span>
+                <span>O</span>
+                <span>A</span>
+                <span>D</span>
+                <span>I</span>
+                <span>N</span>
+                <span>G</span>
+            </p>
+        </div>
+    );
+}
